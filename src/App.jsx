613634bc08ce9335e202db783e69a19d5bbc3f8b
tle <tlemakuz@gmail.com>
2024-06-05 14:19:41 +0700

    feat:rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import MoviesList from './components/MoviesList';
+import React from "react";
 
-function App() {
+  function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+      <h1 className="movie-list-header">Movie List Section</h1>
+        <MoviesList />
       </section>
     </div>
   );
